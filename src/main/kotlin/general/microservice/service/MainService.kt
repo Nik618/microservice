81cbaa39f306e83d48d95c53aaeda02afe364057
Nik618 <trinax01@gmail.com>
2022-04-02 16:53:19 +0300

    02.04_16:53

--- src/main/kotlin/general/microservice/service/MainService.kt
+++ src/main/kotlin/general/microservice/service/MainService.kt
@@ -1,6 +1,8 @@
 package general.microservice.service
 
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import general.microservice.dto.Request
 import general.microservice.dto.Value
 import general.microservice.pojos.ValCurs
 import general.microservice.repository.MainRepository
@@ -10,7 +12,6 @@ import org.simpleframework.xml.Serializer
 import org.simpleframework.xml.core.Persister
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RestController
-
 
 @RestController
 class MainService {
@@ -23,42 +24,36 @@ class MainService {
     @PostMapping("/api/1")
     fun greeting(@RequestBody request: String?): Value {
 
-//        val parts: Array<MainRequest> = gson.fromJson(request, object : TypeToken<Array<MainRequest>>() {}.type)
-//        parts.forEachIndexed {
-//                firstValue, lastValue -> println("> Item ${firstValue}:\n${lastValue}")
-//        }
 
-        val serializer: Serializer = Persister()
-        val valCurs = serializer.read(ValCurs::class.java, request)
+        val parts = gson.fromJson(request, Request::class.java)
+
+
+//        val serializer: Serializer = Persister()
+//        val valCurs = serializer.read(ValCurs::class.java, request)
 
         var budget = 10000000.0
         var countShares = 0
         var countUp = 0;
         var countDown = 0;
-        //var up = false
-        for (i in 0 until valCurs.list.size-2) {
 
-            //(valCurs.list[i+2].value?.toDouble()!! - valCurs.list[i+3].value?.toDouble()!!)
-//            if ((valCurs.list[i].value?.toDouble()!! > valCurs.list[i+1].value?.toDouble()!!)
-//                && (valCurs.list[i+1].value?.toDouble()!! > valCurs.list[i+2].value?.toDouble()!!)
-//                && (valCurs.list[i+2].value?.toDouble()!! < valCurs.list[i+3].value?.toDouble()!!))
+        for (i in 0 until parts.close.size-2) {
 
-            if ((valCurs.list[i].value?.toDouble()!! - valCurs.list[i+1].value?.toDouble()!!)/valCurs.list[i].value?.toDouble()!! > 0.1) {
-                budget += countShares*valCurs.list[i+2].value?.toDouble()!!
+            if ((parts.close[i] - parts.close[i+1])/parts.close[i] > 0.1) {
+                budget += countShares*parts.close[i+2]
                 countShares = 0
             }
 
             if (countUp + countDown > 12) {
-                budget += countShares*valCurs.list[i].value?.toDouble()!!
+                budget += parts.close[i]
                 countShares = 0
             }
 
-            if (valCurs.list[i].value?.toDouble()!! > valCurs.list[i+1].value?.toDouble()!!) {
+            if (parts.close[i] > parts.close[i+1]) {
                 countDown++
 
-                if (countUp >= 3 && countShares != 0 && (valCurs.list[i].value?.toDouble()!! - valCurs.list[i+1].value?.toDouble()!!)/valCurs.list[i].value?.toDouble()!! > 0.02) {
+                if (countUp >= 3 && countShares != 0 && (parts.close[i] - parts.close[i])/parts.close[i+2] > 0.02) {
                     countShares--
-                    budget += valCurs.list[i+2].value?.toDouble()!!
+                    budget += parts.close[i+2]
                 }
 
                 countUp = 0
@@ -69,7 +64,7 @@ class MainService {
 
                 if (countDown >= 2) {
                     countShares++
-                    budget -= valCurs.list[i+2].value?.toDouble()!!
+                    budget -= parts.close[i+2]
                 }
 
                 countDown = 0
@@ -77,6 +72,6 @@ class MainService {
         }
         println(budget)
         println(countShares)
-        return Value(((budget + countShares * valCurs.list[valCurs.list.size-1].value?.toDouble()!!)-10000000)/100000000)
+        return Value(((budget + countShares * parts.close[parts.close.size-1])-10000000)/10000000*100)
     }
 }
