19eb5ae144573843d7c4e08d4e009b85b7d2575b
Nik618 <trinax01@gmail.com>
2022-06-19 22:04:59 +0300

    19.06-final

--- src/main/kotlin/general/microservice/MainApplication.kt
+++ src/main/kotlin/general/microservice/MainApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MainApplicationTests
+class MainApplication
 
 fun main(args: Array<String>) {
-    runApplication<MainApplicationTests>(*args)
+    runApplication<MainApplication>(*args)
 }
